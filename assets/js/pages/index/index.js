03f6ad947e22f5cac73f60b64f7356bfe19c2e6e
Pedro Euzebio <pedroeuzebio.contato@gmail.com>
2025-04-20 19:17:02 -0300

    Adiciona mudança de imagem do cabeçalho quando o tema é alterado

--- assets/js/pages/index/index.js
+++ assets/js/pages/index/index.js
@@ -3,14 +3,19 @@ const switchButton = document.querySelector("#switch #switch-button");
 const switchButtonIcon = document.querySelector(
   "#switch #switch-button #icon-button-switch"
 );
+const img = document.querySelector("header img");
 
 const toggleMode = () => {
   body.classList.toggle("light");
 
   if (body.classList.contains("light")) {
     switchButtonIcon.setAttribute("name", "sunny-outline");
+
+    img.setAttribute("src", "./assets/images/pages/index/logo-light.png");
   } else {
     switchButtonIcon.setAttribute("name", "moon-outline");
+
+    img.setAttribute("src", "./assets/images/pages/index/logo.png");
   }
 };
 
